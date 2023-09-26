9f6d4a4e83a5d3b124219396c7677f353db6aab8
AlekseyAfanasyev <Aleksey.afan1@yandex.ru>
2023-09-26 20:37:54 +0300

    update

--- cmd/passports/main.go
+++ cmd/passports/main.go
@@ -1,1 +1,12 @@
-package passports
+package main
+
+import (
+	"BMSTU_RIP/internal/api"
+	"log"
+)
+
+func main() {
+	log.Println("Application starts!")
+	api.StartServer()
+	log.Println("Application terminated!")
+}
